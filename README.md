1ea411a06855387150a0ffc0c8a2c218e4e3aad5
Oriaje <mthrice@hotmail.com>
2022-02-21 21:15:45 +1000

    finialising the project

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Week4HomeWork
+
 Building a timed coding quiz with multiple-choice questions. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean, polished, and responsive user interface.
+
+## Application Usage
+
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and my score
+
+```
+
+## Mock-Up
+
+The following animation demonstrates the application functionality:
+
+![A user clicks through an interactive coding quiz, then enters initials to save the high score before resetting and starting over.](./Assets/Image/04-web-apis-homework-demo.gif)
+
+© 2021 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
+```
--- js/script.js
+++ js/script.js
@@ -1,19 +1,94 @@
 var currentPage;
+// declaring the vairables for the quiz questions
+const questions = [
+  {
+    id: "question-1",
+    text: "How to code",
+    options: ["Try this option", "How to try", "option3", "answer"],
+    answer: "answer",
+  },
+  {
+    id: "question-1",
+    text: "How to code 1",
+    options: ["Try this option", "How to try", "option3", "answer"],
+    answer: "answer",
+  },
+  {
+    id: "question-2",
+    text: "How to code 2",
+    options: ["Try this option", "How to try", "option3", "answer"],
+    answer: "answer",
+  },
+  {
+    id: "question-3",
+    text: "How to code 3",
+    options: ["Try this option", "How to try", "option3", "answer"],
+    answer: "answer",
+  },
+];
+// declaring the variables
+var currentQuestionIndex = 0;
+var timerClock;
+var totalTime = 60;
+const timePenalty = 10;
+//setting the function
+function getQuestion(index) {
+  const question = questions[index];
 
-function setCurrentPage(elementId) {
-  //hide current page
-  if (currentPage) {
-    currentPage.style.display = "none";
-  }
-  currentPage = document.getElementById(elementId);
+  let html = `<div id="${question.answer}" class="question-container">
+    <p class="question-title">${question.text}</p>
+    <ul class="question-options">`;
 
-  currentPage.style.display = "block";
+  question.options.map((option, index) => {
+    html += `<li onclick="questionOptionSelected('${option}')">${
+      index + 1
+    } ${option}</li>`;
+  });
+
+  return html;
 }
 
-function onPageLoad() {
-  setCurrentPage("welcome-section");
+function questionOptionSelected(option) {
+  console.log(option);
+
+  ++currentQuestionIndex;
+
+  if (currentQuestionIndex < questions.length) {
+    let questionContainer = document.getElementById("question-section-content");
+
+    let html = getQuestion(currentQuestionIndex);
+
+    const isCorrect = option == questions[currentQuestionIndex].answer;
+
+    const questionResponse = isCorrect ? "Correct!" : "Wrong!!";
+    html += ` </ul>
+    <div class="answer-container">
+    <p>${questionResponse}</p>
+    </div>
+    </div>`;
+
+    questionContainer.innerHTML = html;
+
+    if (!isCorrect) {
+      totalTime = totalTime - timePenalty;
+    }
+  }
 }
 
 function startQuiz() {
-  setCurrentPage("question1-section");
+  const welcomeSection = document.getElementById("welcome-section");
+  welcomeSection.style.display = "none";
+
+  document.getElementById("question-sections").style.display = "block";
+
+  let questionContainer = document.getElementById("question-section-content");
+
+  questionContainer.innerHTML = getQuestion(currentQuestionIndex);
+
+  timerClock = setInterval(() => {
+    let clock = document.getElementById("clock");
+
+    totalTime = totalTime - 1;
+    clock.innerHTML = totalTime;
+  }, 1000);
 }
